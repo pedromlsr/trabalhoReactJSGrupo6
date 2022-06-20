b04db1fd04ffbe66056ec7fc56b20d15986b3684
Fabiano Rodrigues <fabianorr87@hotmail.com>
2022-06-20 16:16:19 -0300

    Estilização cadastro cliente

--- trabalho-react-grupo-6/src/Components/CadastroCliente/style.js
+++ trabalho-react-grupo-6/src/Components/CadastroCliente/style.js
@@ -3,18 +3,15 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-`;
-
-export const Titulo = styled.h1`
-  margin-top: 1rem;
-  margin-bottom: 0.5rem;
+  margin-top: -4rem;
+  padding-bottom: 3rem;
 `;
 
 export const Formulario = styled.form`
-  padding: 3rem;
+  padding: 1rem;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: left;
   color: #f7b048;
   font-weight: bold;
   background: #2b2e51;
@@ -23,17 +20,32 @@ export const Formulario = styled.form`
   box-shadow: 2px 2px 10px 1px #f7b048;
 `;
 
+export const Titulo = styled.h1`
+  background: #F7B048;
+  color: #2B2E51;
+  padding: 0.5rem;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  border-radius:0 0.5rem 2rem 0;
+  width: 40%;
+  height: auto;
+  margin-top: 1rem;
+  margin-bottom: 0.5rem;
+  margin-left: -1rem;
+`;
+
 export const ContainerInterno = styled.div`
   display: flex;
   align-items: flex-start;
+  justify-content: ${(props) => props.alinhamento};
 `;
 
 export const Input = styled.input`
   height: 2.5rem;
   width: 20rem;
+  width: ${(props) => props.largura};
   border-radius: 10px;
   border: 2px solid #2b2e51;
-  margin: 0.1rem;
+  margin: 0.3rem;
   &:focus {
     border: 4px solid #f7b048;
   }
@@ -45,9 +57,10 @@ export const InputButton = styled.input`
   height: 2rem;
   width: 10rem;
   border-radius: 10px;
-  border: 2px solid #2b2e51;
+  border: none;
   margin: 1rem;
   &:hover {
     background-color: #f7b048;
   }
+
 `;
