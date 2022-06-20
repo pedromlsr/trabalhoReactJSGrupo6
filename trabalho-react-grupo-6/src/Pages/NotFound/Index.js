c811b50c75b0e5012a5b877927fa46ae59737912
Fabiano Rodrigues <fabianorr87@hotmail.com>
2022-06-20 19:18:17 -0300

    Estilização Page Not Found

--- trabalho-react-grupo-6/src/Pages/NotFound/Index.js
+++ trabalho-react-grupo-6/src/Pages/NotFound/Index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-export const NotFound = () =>{
+import React from "react";
+import { Link } from "react-router-dom";
 
-    return(
-        <>
-        <h1>Página não encontrada</h1>
-        </>
-    )
-}
+import LogoImg from "../../Assets/Img/logo-cor.png";
+
+import { Container, Titulo, TextoLink, Texto, Logo } from "./style";
+export const NotFound = () => {
+  return (
+    <Container>
+      <Logo src={LogoImg}></Logo>
+      <Texto>Sorry!</Texto>
+      <Titulo>Ops... não encontramos o recurso que você procura</Titulo>
+      <Texto>
+        Verifique se o endereço que deseja está escrito corretamente ou volte
+        para nossa{" "}
+        <Link to="/">
+          <TextoLink>página inicial</TextoLink>
+        </Link>{" "}
+        e use os menus de navegação para uma seção específica.
+      </Texto>
+    </Container>
+  );
+};
