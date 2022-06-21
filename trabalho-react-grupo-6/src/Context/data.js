93380d1824e92de1f1dacd6371547909817d31ca
Fabio Freitas <fabiof_costa1@hotmail.com>
2022-06-21 14:37:11 -0300

    Funcionalidade de Post do Pedido

--- trabalho-react-grupo-6/src/Context/data.js
+++ trabalho-react-grupo-6/src/Context/data.js
@@ -7,12 +7,9 @@ const CartItensContext = createContext();
 const CartItensProvider = (props) => {
     const [cartItens, setCartItens] = useState([]);
 
-
     function addItem(produto) {
         setCartItens([...cartItens, produto])
-    }
-    console.log("Contexto", cartItens);
-
+    }    
 
     return (
         <CartItensContext.Provider
