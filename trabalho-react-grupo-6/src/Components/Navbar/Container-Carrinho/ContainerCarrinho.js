a4e4089ec18a8f7472bde051f39b9583dbacdf01
Fabio Freitas <fabiof_costa1@hotmail.com>
2022-06-21 15:13:12 -0300

    Contador do pedido só aparece quando add item

--- trabalho-react-grupo-6/src/Components/Navbar/Container-Carrinho/ContainerCarrinho.js
+++ trabalho-react-grupo-6/src/Components/Navbar/Container-Carrinho/ContainerCarrinho.js
@@ -1,17 +1,19 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Container, CarrinhoImg, Logo, Contador, Quantidade } from "./style";
 
 import Carrinho from "../../../Assets/Icons/carrinho-de-compras.svg";
 
-import {CartItensContext} from "../../../Context/data"
+import { CartItensContext } from "../../../Context/data"
 export const ContainerCarrinho = () => {
   let navigate = useNavigate();
 
-  function handleNavigate(){
+  function handleNavigate() {
     navigate("/pedido");
   }
+
+
 
   const { cartItens } = useContext(CartItensContext)
   return (
@@ -19,9 +21,11 @@ export const ContainerCarrinho = () => {
       <CarrinhoImg onClick={handleNavigate}>
         <Logo src={Carrinho} />
       </CarrinhoImg>
-      <Contador>
-        <Quantidade>{cartItens.length}</Quantidade>
-      </Contador>
+      {cartItens.length > 0 ? (
+        <Contador>
+          <Quantidade>{cartItens.length}</Quantidade>
+        </Contador>
+      ) : null}
     </Container>
   );
 };
