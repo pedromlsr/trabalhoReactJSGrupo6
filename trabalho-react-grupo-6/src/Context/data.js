5e765cdecd13c62550ab260787b2286360358c32
Fabio Freitas <fabiof_costa1@hotmail.com>
2022-06-21 15:31:42 -0300

    Pedido não permite 2 produtos iguais

--- trabalho-react-grupo-6/src/Context/data.js
+++ trabalho-react-grupo-6/src/Context/data.js
@@ -8,6 +8,7 @@ const CartItensProvider = (props) => {
     const [cartItens, setCartItens] = useState([]);
 
     function addItem(produto) {
+        if(!cartItens.includes(produto))
         setCartItens([...cartItens, produto])
     }    
 
