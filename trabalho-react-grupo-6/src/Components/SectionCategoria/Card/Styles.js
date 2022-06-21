6855d8c3f2b4f061c4ce57398e3ef753fb05fa20
Fabiano Rodrigues <fabianorr87@hotmail.com>
2022-06-21 16:34:45 -0300

    Estilização do card de produto

--- trabalho-react-grupo-6/src/Components/SectionCategoria/Card/Styles.js
+++ trabalho-react-grupo-6/src/Components/SectionCategoria/Card/Styles.js
@@ -8,9 +8,16 @@ export const CardArea = styled.div`
   padding: 1rem ;
   flex:none;
   margin:0 1.3rem;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  box-shadow: 2px 3px 4px 1px #00000050,
+    inset 2px 2px 4px 0px #fff,
+    inset -2px -2px 4px 2px #00000050;
   &:hover{
     transform:scale(1.05);
-    box-shadow:0.5rem 0.5rem 0.5rem #F7B048;
+    box-shadow:0.5rem 0.5rem 0.5rem #F7B04850,
+      2px 3px 4px 1px #00000050,
+      inset 2px 2px 4px 0px #fff,
+      inset -2px -2px 4px 2px #00000050;
   }
   
 .content{
@@ -27,25 +34,34 @@ export const CardArea = styled.div`
   align-items: center ;
   border-radius: 1rem ;
   background-color: #eeee ;
-  height: 80% ;  
+  overflow: hidden;
+  height: 80% ; 
+  box-shadow: -3px -3px 4px 2px #00000075;
+  &:hover img {
+    position:absolute;
+    border: 5px solid #686DB1;
+  }
 }
 
 img {
   width:100%;
-  height:100% ;
+  height:100%;
   object-fit: cover ;
   border-radius: 1rem ;
 }
 
 .nomeProduto, .valor{
 text-align: center ;
-color:#2B2E51 ;
+color:#F7B048 ;
 font-size:1.3rem;
 font-weight: bolder ;
 margin-top:0.5rem;
+text-shadow: 2px 2px 2px #000000;
 }
 .valor{
   color:#EE7057;
+  text-shadow: 2px 2px 2px #000000;
+
 }
 
 button {
@@ -57,6 +73,9 @@ button {
   color:#EE7057; 
   margin-top:1rem;
   border-radius: 1rem ;
+  box-shadow: 2px 3px 4px 1px #00000050,
+    inset 2px 2px 4px 0px #fff,
+    inset -2px -2px 4px 2px #00000050;
   &:hover{
     background-color:#F7B048 ;
     color: #2B2E51;    
